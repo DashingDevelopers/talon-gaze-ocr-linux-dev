b0c471ae0e5609e9cc7c4acf43fff15cfe196645
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2024-04-03 22:22:49 +0100

    hardcode tesseract paths

--- gaze_ocr_talon.py
+++ gaze_ocr_talon.py
@@ -244,7 +244,10 @@ def reload_backend(name, flags):
         if setting_ocr_use_talon_backend and not ocr:
             logging.info("Talon OCR not available, will rely on external support.")
         ocr_reader = screen_ocr.Reader.create_fast_reader(
-            radius=settings.get("user.ocr_gaze_point_padding"), homophones=homophones
+            radius=settings.get("user.ocr_gaze_point_padding"),
+            homophones=homophones,
+            tesseract_data_path = r"/usr/share/tesseract-ocr/4.00/tessdata",
+            tesseract_command = r"/usr/bin/tesseract"
         )
     gaze_ocr_controller = gaze_ocr.Controller(
         ocr_reader,
